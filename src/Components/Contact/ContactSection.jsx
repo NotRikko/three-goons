f8e431799935b2df3ff71a3f2c1e95fc120c8975
Rikko <bulletbill51@yahoo.com>
2024-04-15 19:10:38 -0700

    Added boilerplate content to contact page

--- src/Components/Contact/ContactSection.jsx
+++ src/Components/Contact/ContactSection.jsx
@@ -1,23 +1,36 @@
+import Rikko from '../../assets/ennaur323.jpg'
+import Zhou from '../../assets/coww.jpg'
+import Chris from '../../assets/valorant.png'
+
 function ContactSection() {
     return (
         <div id="contactSection">
             <div className="profile">
-                <h2>Zhoucow</h2>
-                <h4>Email</h4>
-                <h4>@zhoucow</h4>
-                <p>A cow from Walnut Creek.</p>
+                <div className="profileInfo">
+                    <h2>Zhoucow</h2>
+                    <h4>Email</h4>
+                    <h4>@zhoucow</h4>
+                    <p>A cow from Walnut Creek.</p>
+                </div>
+                <img src={Zhou}></img>
             </div>
             <div className="profile">
-                <h2>Datlouguy</h2>
-                <h4>Email</h4>
-                <h4>@ch.lou</h4>
-                <p>A dat lou guy.</p>
+                <div className="profileInfo">
+                    <h2>Datlouguy</h2>
+                    <h4>Email</h4>
+                    <h4>@ch.lou</h4>
+                    <p>A dat lou guy.</p>
+                </div>
+                <img src={Chris}></img>
             </div>
             <div className="profile">
-                <h2>Rikko</h2>
-                <h4>Email</h4>
-                <h4>@notrikko</h4>
-                <p>He is not Rikko</p>
+                <div className="profileInfo">
+                    <h2>Rikko</h2>
+                    <h4>Email</h4>
+                    <h4>@notrikko</h4>
+                    <p>He is not Rikko</p>
+                </div>
+                <img src={Rikko}></img>
             </div>
             
         </div>
